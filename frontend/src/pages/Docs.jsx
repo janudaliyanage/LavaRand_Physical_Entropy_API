556738600dfa4bf88227c24d9bd6143e6e4f9955
Godapitiye L .J. V. <jliyanage205@gmail.com>
2026-07-08 01:21:30 +0530

    fix Docs.jsx file

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -69,6 +69,7 @@ export default function Docs() {
     });
 
     return () => observer.disconnect();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const sidebarSections = [
